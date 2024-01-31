87a5d9f9fdd997322a291d07a4dddf008d720146
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-31 16:06:26 +0100

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         PlayerWarrrior playerWar1 = new PlayerWarrrior(100, 200);
         
-        output("Hallo Spieler, Gesundheit: " + playerWar1.getMaxHp() + " HP. Deine Mana: " + playerWar1.getMaxMana()+ "Mana");
+        output("Hallo Spieler, Gesundheit: " + playerWar1.getMaxHp() + " HP. Deine Mana: " + playerWar1.getMaxMana()+ " Mana");
     }
 
 
